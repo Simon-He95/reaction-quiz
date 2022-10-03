7fbb5865da0d23a54a7b250f2d88cddebe37df0b
Simon He <hejian1@xd.com>
2022-10-03 23:01:51 +0800

    chore: update

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -5,11 +5,12 @@ import {
   animationFrameWrapper,
   createElement,
   insertElement,
-  useIntersectionObserver,
 } from "simon-js-tool";
 const props = defineProps<{ modelValue: boolean }>();
 const top = ref(0);
 const left = ref(0);
+
+const blood = ref(60);
 
 // to do 检测物体对于x轴和y轴的垂线同时重叠证明碰撞
 
@@ -128,6 +129,12 @@ function generateObject() {
   const distanceX = lat2 - lat1;
   const distanceY = lng2 - lng1;
   const stop = animationFrameWrapper(() => {
+    console.log(collisionDetection(div, ".target"));
+    if (collisionDetection(div, ".target")) {
+      // 掉血
+      blood.value -= 20;
+      return stop();
+    }
     if (Math.floor(lat1) === Math.floor(lat2)) return stop();
 
     lat1 += distanceX / 100;
@@ -143,10 +150,45 @@ onMounted(() => {
     generateObject();
   }, 1000);
 });
+
+function isStr(o: any): o is string {
+  return typeof o === "string";
+}
+function collisionDetection(
+  o1: string | HTMLElement,
+  o2: string | HTMLElement
+) {
+  const obj1: HTMLElement = isStr(o1) ? document.querySelector(o1)! : o1;
+  const obj2: HTMLElement = isStr(o2) ? document.querySelector(o2)! : o2;
+  if (!obj1 || !obj2) return;
+  const left1_start = obj1.offsetLeft;
+  const left1_end = obj1.offsetLeft + obj1.offsetWidth;
+  const left2_start = obj2.offsetLeft;
+  const left2_end = obj2.offsetLeft + obj2.offsetWidth;
+  const top1_start = obj1.offsetTop;
+  const top1_end = obj1.offsetTop + obj1.offsetHeight;
+  const top2_start = obj2.offsetTop;
+  const top2_end = obj2.offsetTop + obj2.offsetHeight;
+  // 判断是否碰撞
+  return !(
+    left1_start > left2_end ||
+    left1_end < left2_start ||
+    top1_start > top2_end ||
+    top1_end < top2_start
+  );
+}
+const bloodColor = computed(() => {
+  if (blood.value >= 80) return "bg-green";
+  if (blood.value >= 60) return "bg-yellow";
+  return "bg-red";
+});
 </script>
 
 <template>
   <main font-sans p="x-4 y-10" h-full text="center gray-700 dark:gray-200">
+    <div w-100 border-1 border-lightgray border-rd-2 h-10>
+      <div :class="[bloodColor]" :style="{ width: blood + '%' }" h-full></div>
+    </div>
     <div
       w-10
       h-10
